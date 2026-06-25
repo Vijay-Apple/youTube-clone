b5b372e72f76ac1064260afb125760aaccbc6b99
Vijay Rajput <vijayraj639726@gmail.com>
2026-06-25 20:47:11 +0530

    Initial commit

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -6,17 +6,17 @@ import jwt from 'jsonwebtoken'
 export const verifyJwt = asyncHandler(async (req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("authorization")?.replace("Bearer ", "")
-    
+
         if (!token) {
             throw new ApiError(401, "unAuthorized request");
         }
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    
+
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
         if (!user) {
             throw new ApiError(401, "Invalid Access Token")
         }
-    
+
         req.user = user
         next()
     } catch (error) {
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -54,7 +54,6 @@ userSchema.pre("save", async function () {
 
 /*******   COMPARE PASSWORD   ******* */
 
-
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
